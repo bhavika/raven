969fffdcae8c10906705aabe136c57d6d43ccfba
Bhavika Tekwani <bhavicka.7992@gmail.com>
2017-09-02 00:56:22 -0400

    Refactoring Raven class

--- raven/commands.py
+++ raven/commands.py
@@ -14,34 +14,35 @@ def follow(filename):
     r = Raven()
     artist_ids = r.search_artist_ids(filename)
     data = {}
-
+    payload_size = 49
     size = len(artist_ids)
 
-    if size < 49:
+    if size < payload_size:
         data['ids'] = artist_ids
         params = (
             ('type', 'artist'),
             ('ids', ','.join(data['ids']))
         )
-        requests.put(url=r.spotify.prefix+follow_endpoint, headers=r.headers, params=params)
+        response = requests.put(url=r.spotify.prefix+follow_endpoint, headers=r.headers, params=params)
+        return response
 
     else:
-        x = [artist_ids[i: i+49] for i in range(0, len(artist_ids), 49)]
+        x = [artist_ids[i: i+payload_size] for i in range(0, len(artist_ids), payload_size)]
         for chunk in x:
             data['ids'] = chunk
             params = (
                 ('type', 'artist'),
                 ('ids', ','.join(data['ids']))
             )
-            requests.put(url=r.spotify.prefix+follow_endpoint, headers=r.headers, params=params)
+            response = requests.put(url=r.spotify.prefix+follow_endpoint, headers=r.headers, params=params)
 
 
 def add_tracks(location, filename, source='library'):
     r = Raven()
+    payload_size = 49
 
     if source == 'cache':
         track_ids = [line.rstrip('\n') for line in open('.cache-{0}-tracks.txt'.format(os.environ['USERNAME']), 'r')]
-
     else:
         if os.path.isfile(filename):
             track_ids = r.search_song_ids(filepath=filename)
@@ -52,9 +53,9 @@ def add_tracks(location, filename, source='library'):
     username = os.environ['USERNAME']
     size = len(track_ids)
 
-    if location == 'playlist':
-        track_uris = [TRACK_URI_FORMAT + track_id for track_id in track_ids]
+    track_uris = [TRACK_URI_FORMAT + track_id for track_id in track_ids]
 
+    if location == 'playlist':
         playlist_title = input("Enter a name for your playlist ")
 
         public = input("Make this playlist public? (yes/no) ").lower()
@@ -67,15 +68,15 @@ def add_tracks(location, filename, source='library'):
 
         endpoint = playlist_add_endpoint.format(user_id=username, playlist_id=playlist_id)
 
-        # Rate limiting - push 50 items per request
-        if size < 49:
+        # Bypass rate limiting - push < 50 items per request
+        if size < payload_size:
             params = (
                 ('position', 0),
                 ('uris', ','.join(track_uris))
             )
-            requests.put(url=r.spotify.prefix + endpoint, headers=r.headers, params=params)
+            requests.post(url=r.spotify.prefix + endpoint, headers=r.headers, params=params)
         else:
-            x = [track_uris[i: i+49] for i in range(0, size, 49)]
+            x = [track_uris[i: i+payload_size] for i in range(0, size, payload_size)]
             for chunk in tqdm(x):
                 params = (
                     ('position', 0),
@@ -84,12 +85,11 @@ def add_tracks(location, filename, source='library'):
                 requests.post(url=r.spotify.prefix+endpoint, headers=r.headers, params=params)
 
     elif location == 'library':
-
-        if size < 49:
+        if size < payload_size:
             r.spotify.current_user_saved_tracks_add(tracks=track_ids)
         else:
             for i in tqdm(range(size)):
-                x = track_ids[i:i+49]
+                x = track_ids[i:i + payload_size]
                 r.spotify.current_user_saved_tracks_add(tracks=x)
 
 
